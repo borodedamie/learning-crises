b1abaf99e80446c31584e00f44797135bcb4976f
Mhee-narh2328 <100017861+Mhee-narh2328@users.noreply.github.com>
2022-09-27 16:03:17 +0100

    made the changes you told me to

--- src/pages/datas/{ContentfulEduData.id}.js
+++ src/pages/datas/{ContentfulEduData.id}.js
@@ -3,22 +3,24 @@ import * as React from 'react'
 import Layout from '../../components/layout/layout'
 import Seo from '../../components/seo'
 import { convertDate } from '../../utils/convertDate'
+import * as dataPageStyles from '../../styling/style.module.css'
 
 const EduDataPage = (props) => {
     console.log(props)
     return (
         <Layout>
-            <section>
-                <div>
+            <section className= {dataPageStyles.container1}>
+                <div  className= {dataPageStyles.shareHeader}>
                     <h4>{ props.data.contentfulEduData.title }</h4>
                 </div>
-                <div>
+                <div className= {dataPageStyles.shareAuthor}>
                     <p>Posted on { convertDate(props.data.contentfulEduData.createdAt) }</p>
                 </div>
-                <div>
+                <div className= {dataPageStyles.shareImage}>
                     <img 
                         alt='infographics image'
                         src={ props.data.contentfulEduData.infographics.url }
+                        className = {dataPageStyles.shareImagee}
                     />
                 </div>
             </section>
